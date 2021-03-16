ea22e5378a391352deaaed6e7b5dbf767ea9a93f
Hderoche <hugo.deroche@gmail.com>
2021-03-16 15:53:32 +0100

    update cours models users mongo

--- server/models/user-queries.js
+++ server/models/user-queries.js
@@ -1,8 +1,38 @@
 import User from './user-models.js'
 
+
+// CRUD
+// Create
+// Retrieve
+// Update
+// Delete
+
+/**
+ * Créer un utilisateur dans la base de données
+ * 
+ * @function
+ * @async
+ * 
+ * @param {import:('./user-models').UserData} userData
+ * @returns {Promise.<UserMongooseDocuments>}
+ * 
+ */
 export function createUser (userData) {
     const user = new User(userData)
+    user.save
     return user.save().then(() => user)
+}
+
+export function getUserById(id){
+    return User.findById(id)
+}
+
+export function getUserByLogin(login){
+    return User.findOne({ login })
+}
+
+export function getUsers(){
+    return User.find()
 }
 
 export function modifyUser(userData) {
@@ -10,4 +40,8 @@ export function modifyUser(userData) {
     .then(user => {
         // logic to modify the user
     })
+}
+
+export function deleteUser (user) {
+    User.deleteOne({ id: user.id })
 }
