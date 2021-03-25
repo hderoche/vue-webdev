000cc40df9697404efbb47481a3fdb1ee9062eca
Hderoche <hugo.deroche@gmail.com>
2021-03-25 15:40:30 +0100

    App menu works responsive

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,7 +11,7 @@
             </a>
           </div>
           <div class="-mr-2 -my-2 md:hidden">
-            <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
+            <button type="button" :class="{active: showMenu}" v-on:click.prevent="closeMenu()" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
               <span class="sr-only">Open menu</span>
               <!-- Heroicon name: outline/menu -->
               <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -46,7 +46,7 @@
           From: "opacity-100 scale-100"
           To: "opacity-0 scale-95"
       -->
-      <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right z-40 md:hidden">
+      <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right z-40 md:hidden" :class="{active: showMenu}">
         <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
           <div class="pt-5 pb-6 px-5">
             <div class="flex items-center justify-between">
